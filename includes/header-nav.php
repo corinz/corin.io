3991bff488493a22127df40dc41cfdc91ac85d34
Corin Ziyadeh <corinziyadeh@MacBook-Pro-2.local>
2018-05-25 23:03:15 -0400

    about.php -> index.php

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -2,7 +2,7 @@
         <header class="masthead">
             <div class="inner">                
                 <nav class="nav nav-masthead justify-content-right">
-                    <a class="nav-link" href="about.php" id="navabout">About</a>
+                    <a class="nav-link" href="index.php" id="navabout">About</a>
                     <a class="nav-link" href="resume.php" id="navresume">Resume</a>
                     <a class="nav-link" href="upperroom.php" id="navupperroom">Upper Room</a>
                 </nav>
